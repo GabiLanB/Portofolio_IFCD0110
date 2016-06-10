ffbb07972a83f5fef7662901192cc942cb4d264d
iGabiDev <iGabiDev@192.168.1.131>
2016-06-10 12:30:48 +0200

    Basic structure almost ready

---
+++ Portofolio/public_html/js/javascript.js
@@ -0,0 +1,11 @@
+function dropDown(){
+    var home = document.getElementById("home");
+    alert(home);
+    /*if (home.style.position == "relative"){
+        drop.style.position = "absolute";
+    } else {
+        drop.style.position = "relative";
+    }*/
+}
+
+
